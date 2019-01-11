996a4b8efe955d8c1e7fe5407bb8f54b7b0551c5
sangee9968 <40237981+sangee9968@users.noreply.github.com>
2019-01-11 22:11:00 +0530

    Update fabonacci_series.py

--- fabonacci_series.py
+++ fabonacci_series.py
@@ -1,11 +1,13 @@
+s=""
 n=int(input())
 a=0
 b=1
-print(b)
+print(b,end=" ")
 for i in range(n-1):
    c=a+b
    #print result
-   print(c)
+   s=s+str(c)+" "
    a=b
    b=c
+print(s.strip())   
      
